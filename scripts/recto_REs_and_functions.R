2a951541786fc230b58e21097c21b6a7d2b88da4
notothen <christiansen.he@gmail.com>
2021-06-11 10:48:28 +0200

    added marker density function

--- scripts/recto_REs_and_functions.R
+++ scripts/recto_REs_and_functions.R
@@ -1,12 +1,18 @@
 #### Source script containing restriction enzymes,
 #### size windows and functions 
 #### for the RRS pilot experiment within RECTO
-## 02/06/2021
+## 11/06/2021
 ## H. Christiansen
-## v3.3
+## v4.0
 
 ## This is a source script containing R objects and functions that do most of the work.
 ## The scripts 01_digests.R and 02_empirical_digests.R depend on this source script.
+
+## load packages required for these function
+#### install them first if you don't have them
+library(scales)
+library(SimRAD)
+library(bioanalyzeR)
 
 #### load restriction enzymes
 #####
@@ -391,5 +397,49 @@ recto_ggsave <- function(name, plot) {
   ggsave(paste0(name, ".eps"), plot, path = here('figures'),
          width = 10, height = 5, units = 'in', dpi = 1200)
   dev.off()
+}
+#####
+
+#### function to calculate marker density
+#### this function calculates how many bp are sequenced in one individual,
+#### provided the number of fragments and assuming that all of these fragments
+#### are sequenced in full and retained through the bioinformatic processing
+#### (that means in theory each fragment must receive at least 1x sequencing coverage,
+####  but more realistically minimum 10x coverage or so to pass bioinformatics)
+#### this also assumes that each fragment is at least as large as two times the read-length
+#####
+marker_density <- function(fragments, genome_size, sequencer = "HiSeq2500", paired_end, SNP_density){
+  ## check whether paired end or single end sequences should be estimated
+  if (paired_end == T) {
+    ratio <- 2
+  } else if (paired_end == F) {
+    ratio <- 1
+  } else {
+    stop("paired_end must be either true or false")
+  }
+  ## define read length
+  if (sequencer == "HiSeq4000") {
+    read_length <- read_length_hiseq4000
+  } else if (sequencer == "HiSeq2500") {
+    read_length <- read_length_hiseq2500
+  } else if (sequencer == "NovaSeq") {
+    read_length <- read_length_novaseq
+  } else {
+    stop("sequencer must be either HiSeq2500, HiSeq4000 or NovaSeq")
+  }
+  results <- data.frame()
+  sequenced_bases <- ratio*read_length*fragments
+  density <- sequenced_bases*SNP_density
+  portion <- round((sequenced_bases/genome_size), digits = 2)
+  everyother <- genome_size/density
+  results <- data.frame(format(fragments, big.mark = ","), format(sequenced_bases, big.mark = ","),
+                        format(density, big.mark = ","), percent(portion), paste(round(everyother, digits = 0), "bp"))
+  ## make the results a bit more appealing/easier to read
+  names(results)[1] <- "number_of_fragments"
+  names(results)[2] <- "number_of_bases_sequenced"
+  names(results)[3] <- "number_of_SNPs"
+  names(results)[4] <- "portion_of_genome_sequenced"
+  names(results)[5] <- "one_SNP_every"
+  return(results)  
 }
 #####
